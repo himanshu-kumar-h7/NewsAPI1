7c377916b94ae7db0bdbcb03f8f90e2c631f6f88
Himanshu <himanshu@gmail.com>
2024-05-07 00:47:31 +0530

    Components updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-
-import './App.css';
+import  './App.css'
 
 import React, { Component } from 'react'
+import News from './Components/News'
+import Navbar from './Components/Navbar'
 
-export class App extends Component {
+export default class App extends Component {
+
+  
   render() {
     return (
       <div>
-        <>
         
-        This is first classxxdd</>
+       < Navbar/>
+       <News/>
+     <News/>
         
       </div>
     )
   }
 }
 
-export default App
+
 
