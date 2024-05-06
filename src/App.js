b9fdcb8d9496657fb5778a363932bc4f92208a6f
Himanshu <himanshu@gmail.com>
2024-05-07 01:23:42 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import  './App.css'
+
+import './App.css';
 
 import React, { Component } from 'react'
-import News from './Components/News'
-import Navbar from './Components/Navbar'
 
 export default class App extends Component {
-
-  
   render() {
     return (
-      <>
-      < Navbar/>
-       <News/>
-     <News/>
-      </>
+      <div>
+        <>
         
-       
         
-      
+        
+        </>
+        
+      </div>
     )
   }
 }
-
-
 
