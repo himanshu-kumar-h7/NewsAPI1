db2ee41b787818a319180d99df4b4bf00acf5836
Himanshu <himanshu@gmail.com>
2024-05-07 01:10:57 +0530

    s

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ export default class App extends Component {
   
   render() {
     return (
-      <div>
-        
-       < Navbar/>
+      <>
+      < Navbar/>
        <News/>
      <News/>
+      </>
         
-      </div>
+       
+        
+      
     )
   }
 }
