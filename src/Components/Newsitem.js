b88909d52d5b15c947364bc8865c476d784a8d5d
Himanshu <himanshu@gmail.com>
2024-05-07 14:43:18 +0530

    added

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+
 
 export default class Newsitem extends Component {
-  static propTypes = {
-
-  }
+  
 
   render() {
+    let { title, descrption } = this.props;
     return (
       <div>
-        This is Item 1
+        
+        
+             
+             <div className="card" style={{ width: "18rem" }}>
+          <img src='https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg' className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+            {descrption}
+            </p>
+            <a href="/" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        
+        </div>
+
+        
       </div>
-    )
+    );
   }
 }
-
-
